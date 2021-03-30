19794714e2bbc1bd9ee0f90f7e28e9c92c8844ba
Анастасия Полякова <78700473+Poliakkova@users.noreply.github.com>
2021-03-31 02:44:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function () {
 	$('.open-button').click(function () {
-		$('.nav').toggleClass('show-menu')
-	})
+		$('.nav').toggleClass('show-menu');
+	});
+
+	$('.filter-button').click(function () {
+		$('.left').toggleClass('show-filter');
+	});
+
+	$('.cross').click(function () {
+		$('.left').toggleClass('show-filter');
+	});
+
+	$('.overlay').click(function () {
+		$('.left').toggleClass('show-filter');
+	});
 });
