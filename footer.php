ad7187d883e5b998a635b060dae5727d0d3f7fe8
Анастасия Полякова <78700473+Poliakkova@users.noreply.github.com>
2021-04-07 22:40:50 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer id="footer" class="footer">
+		<div class="container">
+			<div class="nav">
+				<a href="index.html"><img src="img/logo.svg" alt="Toys for boys" class="logo2"></a>
+				<ul class="menu2">
+					<li>
+						<a href="index.html">Главная</a>
+					</li>
+					<li>
+						<a href="toycatalogue.php">Игрушки</a>
+					</li>
+					<li>
+						<a href="shopabout.html">О магазине</a>
+					</li>
+					<li>
+						<a href="https://instagram.com/toysforkids_kiev?igshid=1mqhtjbhmwr9m">Инстаграмм</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+	
+	<!--<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>-->
+	<script src="js/main.js"></script>
+</body>
+</html>
